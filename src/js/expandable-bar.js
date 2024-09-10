b507509de7afa725840d109cae2812d3cbe9d4b0
Coktoco <albert.winiarski6@gmail.com>
2024-09-10 23:25:48 +0200

    - Complete overhaul of 'about me' section = Finished

--- src/js/expandable-bar.js
+++ src/js/expandable-bar.js
@@ -1,36 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const accordion = document.getElementById('accordionProject1'); 
+    const accordion = document.getElementById('accordionProject1');
+    const accordionAbout = document.getElementById('accordionAboutMe');
     const expandableBars = document.querySelectorAll('.text-spacer'); 
 
-    accordion.addEventListener('mouseover', function (event) {
-        if (event.target && event.target.tagName === 'H4') {
-            const correspondingBar = event.target.nextElementSibling; 
-            if (correspondingBar && correspondingBar.classList.contains('text-spacer')) {
-                correspondingBar.style.width = '20%'; 
-            }
-        }
-    });
+    const accordionElements = [accordion, accordionAbout];
 
-    accordion.addEventListener('mouseout', function (event) {
-        if (event.target && event.target.tagName === 'H4') {
-            const correspondingBar = event.target.nextElementSibling; 
-            if (correspondingBar && correspondingBar.classList.contains('text-spacer')) {
-                correspondingBar.style.width = '10%'; 
-            }
-        }
-    });
-
-    accordion.addEventListener('click', function (event) {
-        if (event.target && event.target.tagName === 'H4') {
-            const targetCollapse = document.querySelector(event.target.getAttribute('data-bs-target')); 
-            setTimeout(function () {
-                if (!targetCollapse.classList.contains('show')) {
-                    const correspondingBar = event.target.nextElementSibling; 
-                    if (correspondingBar && correspondingBar.classList.contains('text-spacer')) {
-                        correspondingBar.style.width = '10%'; 
-                    }
+    accordionElements.forEach(accordionElement => {
+        accordionElement.addEventListener('mouseover', function (event) {
+            if (event.target && event.target.tagName === 'H4') {
+                const correspondingBar = event.target.nextElementSibling; 
+                if (correspondingBar && correspondingBar.classList.contains('text-spacer')) {
+                    correspondingBar.style.width = '20%'; 
                 }
-            }, 350);  
-        }
+            }
+        })
     });
+
+    accordionElements.forEach(accordionElement => {
+        accordionElement.addEventListener('mouseout', function (event) {
+            if (event.target && event.target.tagName === 'H4') {
+                const correspondingBar = event.target.nextElementSibling; 
+                if (correspondingBar && correspondingBar.classList.contains('text-spacer')) {
+                    correspondingBar.style.width = '10%'; 
+                }
+            }
+        })
+    });
+    
 });
