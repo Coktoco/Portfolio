f89739b26774169cdc1c481cc1a64e7da35db1a0
Coktoco <albert.winiarski6@gmail.com>
2024-09-11 17:34:26 +0200

    -Still 'fighting' with scrolltrigger..

--- src/js/scrolltrigger.js
+++ src/js/scrolltrigger.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger)
+    gsap.registerPlugin(ScrollTrigger);
 
     let sections = gsap.utils.toArray(".panel");
+    
 
     gsap.to(sections, {
         xPercent: -100 * (sections.length - 1),
         ease: "none",
         scrollTrigger: {
-            start: "-140px",
-            // markers: true,
-            trigger: ".panel-containment",
-            pin: true,
-            scrub: 1,
+            trigger: ".scroll-section", 
+            start: "top top",
+            pin: ".panel-containment", 
+            scrub: 1, 
             snap: 1 / (sections.length - 1),
-            end: () => "+=1" + document.querySelector(".panel-containment").offsetWidth
+            end: () => "+=" + document.querySelector(".panel-containment").offsetWidth 
         }
-    })
-
+    });
 });
