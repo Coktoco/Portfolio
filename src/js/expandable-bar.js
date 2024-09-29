c4a8b8334d0a225b6dcc91dee8ded9318c667e77
Coktoco <albert.winiarski6@gmail.com>
2024-09-30 00:56:51 +0200

    Lots of changes: - Added new project: Photography - Added new animation to title screen using textsplit (very nice)

--- src/js/expandable-bar.js
+++ src/js/expandable-bar.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const accordion2 = document.getElementById('accordionProject2');
     const accordion3 = document.getElementById('accordionProject3');
     const accordion4 = document.getElementById('accordionProject4');
+    const accordion5 = document.getElementById('accordionProject5');
+    const accordion6 = document.getElementById('accordionProject6');
+
     const accordionAbout = document.getElementById('accordionAboutMe');
 
     // ACCORDION ABOUT ME SECTION
@@ -99,6 +102,47 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     accordion4.addEventListener('mouseout', function (event) {
+        if (event.target && event.target.tagName === 'H4') {
+            const correspondingBar = event.target.nextElementSibling; 
+            if (correspondingBar && correspondingBar.classList.contains('text-spacer')) {
+                correspondingBar.style.width = '10%'; 
+            }
+        }
+    });
+
+    // ACCORDION PROJECT BLENDER
+
+    accordion5.addEventListener('mouseover', function (event) {
+        if (event.target && event.target.tagName === 'H4') {
+            const correspondingBar = event.target.nextElementSibling; 
+            if (correspondingBar && correspondingBar.classList.contains('text-spacer')) {
+                correspondingBar.style.width = '20%'; 
+            }
+        }
+    });
+
+    accordion5.addEventListener('mouseout', function (event) {
+        if (event.target && event.target.tagName === 'H4') {
+            const correspondingBar = event.target.nextElementSibling; 
+            if (correspondingBar && correspondingBar.classList.contains('text-spacer')) {
+                correspondingBar.style.width = '10%'; 
+            }
+        }
+    });
+
+
+    // ACCORDION PROJECT PHOTOGRAPHY
+
+    accordion6.addEventListener('mouseover', function (event) {
+        if (event.target && event.target.tagName === 'H4') {
+            const correspondingBar = event.target.nextElementSibling; 
+            if (correspondingBar && correspondingBar.classList.contains('text-spacer')) {
+                correspondingBar.style.width = '20%'; 
+            }
+        }
+    });
+
+    accordion6.addEventListener('mouseout', function (event) {
         if (event.target && event.target.tagName === 'H4') {
             const correspondingBar = event.target.nextElementSibling; 
             if (correspondingBar && correspondingBar.classList.contains('text-spacer')) {
