78a8745040a8f331da72fcfa395174b63b9661ec
Coktoco <albert.winiarski6@gmail.com>
2024-10-05 00:16:52 +0200

    - Remade SplitText completely from ground up by myself to better suit my needs - This change should help boost the performance even more

---
+++ assets/splitTextDiy.js
@@ -0,0 +1,40 @@
+
+const numbers = [];
+const letters = [];
+
+let myName = document.getElementById('my_name').firstElementChild;
+for(let i = 0; i < 6; i++){
+    letters[i] = myName;
+    numbers[i] = i;
+    myName = myName.nextElementSibling;
+}
+
+let surname = document.getElementById('my_surname').firstElementChild;
+for(let i = 6; i < 15; i++){
+    letters[i] = surname;
+    numbers[i] = i;
+
+    surname = surname.nextElementSibling;
+}
+
+function get_random (list) {
+    return list[Math.floor((Math.random()*list.length))];
+}
+
+function randomAppear(array, numbers, interval) {
+    let number = 0;
+    let counter = 0;
+
+    setInterval(() => {
+        if (counter >= array.length)
+            return
+
+        number = get_random(numbers);
+        array[number].style.opacity = 1;
+        numbers.splice(numbers.indexOf(number), 1);
+
+        counter++;
+    }, interval); 
+}
+
+randomAppear(letters, numbers, 70);
